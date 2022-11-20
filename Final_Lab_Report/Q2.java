8a7af2b441bb6d19af7fb17ddbef5dd752a03084
Sourav <111035583+Sourav15-4588@users.noreply.github.com>
2022-11-20 18:36:15 +0600

    Create Q2.java

---
+++ Final_Lab_Report/Q2.java
@@ -0,0 +1,9 @@
+public class simpleobject {
+    public simpleobject(){
+        System.out.println("Hello, simple object is created");
+    }
+    public static void main(String[] args){
+        simpleobject s = new simpleobject();
+        System.out.println("Constractor created");
+    }
+}
