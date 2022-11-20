0a779d19624b5edf339723cf62678e5d1f294018
Sourav <111035583+Sourav15-4588@users.noreply.github.com>
2022-11-20 18:42:44 +0600

    Create q10.java

---
+++ Final_Lab_Report/q10.java
@@ -0,0 +1,34 @@
+public interface Aa {
+    public int meth1(int a);
+    public void meth2(int a);
+}
+
+
+
+public interface Bb {
+    public void show();
+}
+
+
+
+import java.util.*;
+public class MyClass implements Aa, Bb{
+    public int meth1(int a){
+        return a*a;
+    }
+    public void show(){
+        System.out.println("Sourav Kumar Das");
+    }
+    public void meth2(int a){
+        System.out.println(a);
+    }
+
+    public static void main(String args[]){
+        MyClass ob = new MyClass();
+        Scanner s = new Scanner(System.in);
+        int a=s.nextInt();
+        System.out.println("Square of "+a+" = "+ob.meth1(a));
+        ob.meth2(a);
+        ob.show();
+    }
+}
