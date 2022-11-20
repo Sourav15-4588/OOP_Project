68a2c90b80a1983375093ffec5e018cec445b327
Sourav <111035583+Sourav15-4588@users.noreply.github.com>
2022-11-20 18:41:20 +0600

    Create q8.java

---
+++ Final_Lab_Report/q8.java
@@ -0,0 +1,20 @@
+abstract class AbstractExample {
+    public abstract void display();
+    public void showData(){
+        System.out.println("SOURAV KUMAR DAS");
+    }
+}
+
+
+
+public class child extends AbstractExample{
+    public void display(){
+        System.out.println("ID 213-15-4588");
+    }
+
+    public static void main(String args[]){
+        child ob = new child();
+        ob.display();
+        ob.showData();
+    }
+}
