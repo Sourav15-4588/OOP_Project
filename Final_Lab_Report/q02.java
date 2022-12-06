cde5ebb413205f1d9a81e3403cdc85c593a328e3
Sourav <111035583+Sourav15-4588@users.noreply.github.com>
2022-12-06 07:00:01 +0600

    Update q02.java

--- Final_Lab_Report/q02.java
+++ Final_Lab_Report/q02.java
@@ -1,3 +1,7 @@
+/*2. Write a program create a class ‘simpleobject‘. Using constructor, display the message
+“Hello, simple object is created”. 
+*/
+
 public class simpleobject {
     public simpleobject(){
         System.out.println("Hello, simple object is created");
