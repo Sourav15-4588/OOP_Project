52590018081ccd8502476e579fc3cb57e566184c
Sourav <111035583+Sourav15-4588@users.noreply.github.com>
2022-12-06 07:04:39 +0600

    Update and rename q09.java to q_09.java

--- Final_Lab_Report/q_09.java
+++ Final_Lab_Report/q_09.java
@@ -1,3 +1,8 @@
+/*
+9. Write a program to create interface A in this interface we have two method meth1 and
+meth2. Implements this interface in another class named MyClass.
+*/
+
 public interface Aa {
     public int meth1(int a);
     public void meth2(int a);
